a652face883efe6a9b1fa42aea276f75be581d7c
Charles Mwangi <charlimwangi12@gmail.com>
2019-10-24 12:16:43 +0300

    Initial commit

---
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -0,0 +1,42 @@
+package com.pluralsight.candycoded;
+
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
+        ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
+        Picasso.with(this).
+                load(uri).
+                into(candyStoreImageView);
+
+
+    }
+
+    // ***
+    // TODO - Task 2 - Launch the Google Maps Activity
+    // ***
+    TextView mapView = findViewById(R.id.text_view_address);
+//    mapView..setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick (View v){
+//
+//        }
+//    }
+            // ***
+    // TODO - Task 3 - Launch the Phone Activity
+    // ***
+}
